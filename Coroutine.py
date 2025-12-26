e9f7f302d116169ac487222c1c0dc94d03945663
wuguangbin <2567199685@qq.com>
2025-12-26 16:44:33 +0800

    asyncã...await

--- Coroutine.py
+++ Coroutine.py
@@ -1,3 +1,7 @@
+import asyncio
+
+### 1、示例一：生产者函数和消费者函数
+
 # 消费者函数
 # 首次执行时，会返回一个迭代器对象，指针停在函数开头
 def consumer():
@@ -23,7 +27,7 @@ def producer(c):
         print(f"生产者：生产了数字 {n}")
         c.send(n)
     c.close()
-    print("完成所有数据的生产和消费")
+    print("完成所有数据的生产和消费\n\n\n")
 
 # 获取迭代器实例
 print('准备获取迭代器实例')
@@ -32,6 +36,25 @@ print('已经获取迭代器实例\n')
 # 开始生产，将迭代器实例传进去
 producer(c)
 
+
+
+### 示例二：使用 async 和 await 实现协程
+# 使用 async...await 会创建一个协程，本质是一个迭代器
+#（1）单个 async...await
+async def hello(name):
+    print(f"hello {name}!!!(会休眠一秒）")
+    # sleep 可以换成真正的 IO 操作，那么，多个 IO 操作，就可以在同一线程中并发执行
+    await asyncio.sleep(1)
+    print(f"hello {name} again!!!")
+print("准备使用 run 方法执行单个协程")
+asyncio.run(hello('Tom'))
+print("\n\n\n")
+
+#（2）多个 async...await
+print("准备使用 gather 方法多个协程")
+async def mul_coroutine():
+    await asyncio.gather(hello('Jack'), hello('Jill'))
+asyncio.run(mul_coroutine())
 
 
 
